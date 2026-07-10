c80300804163309b7a35aa3afd8c8231796fb82a
John <johnwaweru092@gmail.com>
2026-07-10 16:47:02 +0300

    Created the access token generation

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "MPESA Daraja API";
+use Dotenv\Dotenv;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// MPESA API Keys
+$consumerKey = $_ENV["CONSUMER_KEY"];
+$consumeSecret = $_ENV["CONSUMER_SECRET"];
+
+// Access Token URL
+$accessTokenUrl = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+$headers = ['Content-Type:application/json; charset=utf-8'];
+
+$curl = curl_init($accessTokenUrl);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumeSecret);
+
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+$result = json_decode($result);
+echo $access_token = $result->access_token;
+
