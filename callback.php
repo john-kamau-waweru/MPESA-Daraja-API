b6501f4eb2de3bf537498e281355fe63a328b281
John <johnwaweru092@gmail.com>
2026-07-13 17:23:35 +0300

    Adjusted the callback functionality

--- callback.php
+++ callback.php
@@ -15,3 +15,8 @@ $ResultDesc = $data->Body->stkCallback->ResultDesc;
 $Amount = $data->Body->stkCallback->CallbackMetadata->Item[0]->Value;
 $TransactionID = $data->Body->stkCallback->CallbackMetadata->Item[1]->Value;
 $UserPhoneNumber = $data->Body->stkCallback->CallbackMetadata->Item[4]->Value;
+
+// Check if the transaction is successful
+if ($ResultCode == 0) {
+  // STORE THE TRANSACTION DETAILS IN THE DATABASE
+}
